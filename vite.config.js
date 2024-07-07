00bb4197e4ed2a34f4848398cb55afc5669fb033
Noysen02 <noysen2001@gmail.com>
2024-07-06 23:32:46 -0600

    updatebase

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import handlebars from 'vite-plugin-handlebars';
 import handlerBarsContext from './variables.js';
 
 export default defineConfig({
-    base: "/portafolioactualizado/",
+    base: "/portactualizado/",
     appType: 'mpa',
     build: {
         rollupOptions: {
